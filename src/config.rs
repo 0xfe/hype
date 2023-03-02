97d964283716f768ba2c16df9deb311da9f1b687
Mohit Muthanna Cheppudira <mohit@muthanna.com>
2023-03-02 17:08:25 -0500

    Added server configuration.

--- src/config.rs
+++ src/config.rs
@@ -19,16 +19,68 @@ pub struct Route {
 }
 
 #[derive(Debug)]
+pub enum LogLevel {
+    Debug,
+    Info,
+    Warn,
+    Error,
+}
+
+#[derive(Debug)]
+pub struct Server {
+    pub listen_ip: String,
+    pub port: u32,
+    pub log_level: LogLevel,
+}
+
+#[derive(Debug)]
 pub struct Config {
+    pub server: Server,
     pub routes: Vec<Route>,
 }
 
 impl Config {
     pub fn from_str(config_str: impl AsRef<str>) -> Result<Self, String> {
-        let mut config = Config { routes: vec![] };
+        let mut config = Config {
+            routes: vec![],
+            server: Server {
+                listen_ip: "127.0.0.1".into(),
+                port: 8000,
+                log_level: LogLevel::Info,
+            },
+        };
 
         for document in Deserializer::from_str(config_str.as_ref()) {
             let value = Value::deserialize(document).unwrap();
+
+            let server = value
+                .get("server")
+                .ok_or("missing server section".to_string())?;
+
+            if !server.is_sequence() {
+                return Err("malformed server section".to_string());
+            }
+
+            for s in server.as_sequence().unwrap() {
+                if let Some(listen_ip) = s.get("listen_ip") {
+                    config.server.listen_ip = listen_ip.as_str().unwrap_or("".into()).into();
+                }
+
+                if let Some(port) = s.get("port") {
+                    config.server.port = port.as_u64().unwrap_or(0) as u32;
+                }
+
+                if let Some(log_level) = s.get("log_level") {
+                    match log_level.as_str().unwrap_or("info") {
+                        "debug" => config.server.log_level = LogLevel::Debug,
+                        "info" => config.server.log_level = LogLevel::Info,
+                        "warn" => config.server.log_level = LogLevel::Warn,
+                        "error" => config.server.log_level = LogLevel::Error,
+                        _ => config.server.log_level = LogLevel::Info,
+                    }
+                }
+            }
+
             let routes = value.get("routes").ok_or("missing routes".to_string())?;
 
             if !routes.is_sequence() {
